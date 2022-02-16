6b0919f9e0884fdc6a085a5dc63f5f633a53cf16
Nuuggs <bryluke000@gmail.com>
2022-02-16 15:43:40 +0800

    installed react-router-dom@5.2.0; tried test routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Header from './components/Header.jsx';
 import LandingPage from './components/LandingPage.jsx';
@@ -8,7 +9,32 @@ const App = () => (
   <div className="App flex flex-col items-center">
     <Header />
     <LandingPage />
+    <Router>
+      <div className="body">
+        <nav>
+          <Link to="/test" exact>
+            <div>Test</div>
+          </Link>
+          <Link to="/test2" exact>
+            <div>Test2</div>
+          </Link>
+        </nav>
+
+        <Switch>
+          <Route path="/test2"><Test2 /></Route>
+          <Route path="/test"><Test /></Route>
+        </Switch>
+      </div>
+    </Router>
   </div>
 );
 
 export default App;
+
+const Test = () => (
+  <div>Test</div>
+);
+
+const Test2 = () => (
+  <div>Test2</div>
+);
