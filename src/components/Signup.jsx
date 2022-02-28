7cd2bbbebdd1783dc01ccbcf97fbc2a332d96382
Nuuggs <bryluke000@gmail.com>
2022-02-28 17:03:02 +0800

    finished styling phase 1

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -43,36 +43,36 @@ const Signup = ({ setAuth }) => {
   };
 
   return (
-    <div className="max-w-md w-full m-auto bg-indigo-100 rounded p-5">
+    <div className="max-w-md w-full m-auto bg-huat-50 rounded p-5">
       <form onSubmit={handleSignup}>
         <div>
-          <label className="block mb-2 text-indigo-500" htmlFor="name">
+          <label className="block mb-2 text-lg text-huat-90" htmlFor="name">
             Name
-            <input className="w-full p-2 mb-4 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" name="name" value={username} onChange={(e) => setUsername(e.target.value)} required />
+            <input className="w-full p-2 mb-4 text-huat-40 border-b-4 border-huat-70 outline-none focus:bg-gray-300" name="name" value={username} onChange={(e) => setUsername(e.target.value)} required />
           </label>
         </div>
 
         <div>
-          <label className="block mb-2 text-indigo-500" htmlFor="email">
+          <label className="block mb-2 text-lg text-huat-90" htmlFor="email">
             Email
-            <input className="w-full p-2 mb-2 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="email" name="email" value={email} onChange={handleEmailChange} required />
+            <input className="w-full p-2 mb-2 text-huat-40 border-b-4 border-huat-70 outline-none focus:bg-gray-300" type="email" name="email" value={email} onChange={handleEmailChange} required />
           </label>
           <p className="text-sm text-red-500 font-bold mb-4">{message}</p>
         </div>
 
         <div>
-          <label className="block mb-2 text-indigo-500" htmlFor="password">
+          <label className="block mb-2 text-lg text-huat-90" htmlFor="password">
             Password
-            <input className="w-full p-2 mb-4 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <input className="w-full p-2 mb-4 text-huat-40 border-b-4 border-huat-70 outline-none focus:bg-gray-300" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
           </label>
         </div>
-        <input className="w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-4 rounded" type="submit" value="Sign Up" />
+        <input className="w-full mx-auto mb-4 bg-huat-50 border-2 border-huat-10 hover:bg-huat-80 disabled:opacity-50 disabled:bg-huat-40 mt-4 text-huat-90 font-bold py-2 px-4 rounded-full" type="submit" value="Sign Up" />
       </form>
 
       <footer className="text-center">
         <span className="text-sm">
           Already have an account?
-          <Link to="/login" className="text-indigo-700 hover:text-pink-700"> Log in!</Link>
+          <Link to="/login" className="text-huat-90 hover:text-huat-40 text-sm"> Log in!</Link>
         </span>
       </footer>
     </div>
